8141cb70f584aa150556d261fb8eaca6bdba3919
victorrocha95 <victorrocha.ti@gmail.com>
2022-03-08 15:26:33 -0300

    main

---
+++ funcao_range.py
@@ -0,0 +1,10 @@
+print (range(10))
+print (range(5, 10))
+print (range(10, 0))
+
+i=int(input("Digite um Número"))
+
+if i in range(0,10):
+   print("Está Contido")
+else:
+    print("Não está Contido")
